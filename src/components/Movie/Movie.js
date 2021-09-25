2f82984aee6209efaf8edce75c7a95e41bada95f
Arif-Hossain-ProHero <arif.programminghero@gmail.com>
2021-09-26 05:27:39 +0600

    main section design completed

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -2,18 +2,25 @@ import React from 'react';
 import './Movie.css'
 
 const Movie = (props) => {
-    const { name, director, img} = props.movie;
+    const { name, director, img, ratings,genre, stars,budget} = props.movie;
     return (
-            <div className="card mb-3" style={{maxWidth: "300px"}}>
+            <div className="card mb-3" style={{maxWidth: "310px"}}>
                 <div className="row g-0">
                     <div className="col-md-4">
-                        <img src={img} className="img-fluid rounded-start" alt="..."/>
+                    <img src={img} className="rounded-start" alt="..." />
+                    <div className='ratings-container'>
+                        <p className='ratings'><i class="fas fa-star"></i>{ratings}</p>
+                        <p className='play'><i class="far fa-play-circle"></i> Play trailer</p>
+                    </div>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            <h6 className="card-title">{name}</h6>
+                            <p className='genre'>{genre}</p>
+                            <p className='director'>Directed by: {director}</p>
+                            <p className='stars'><span style={{color:'#212529'}}>Starring:</span> {stars}</p>
+                        <p className='budget'>Budget: ${budget} Million</p>
+                        <button className='add-btn btn btn-primary'>Add to Favourite</button>
                         </div>
                     </div>
                 </div>
